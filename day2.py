797e1c767e3703cc753da6c09559d2e8fd7f0da2
swastikshukla <swastikshukla778@gmail.com>
2024-05-17 12:31:27 +0530

    testing_purpose

--- day2.py
+++ day2.py
@@ -1,4 +1,7 @@
 #hey hello everyone this day 2 is for my testing purpose for the github issues so just checking
 print("hello world")
 name=input("what is the username")
-print("my name is" ,name)
+print("my name is" ,name) 
+x=245
+y=235
+print(x,y)
